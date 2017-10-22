79a137d627c6602327c88895894cbb0e181cc117
Casper Prins <casperprins98@gmail.com>
2017-10-22 13:05:27 +0200

    done

--- seq_to_ncbi_info/bash_info_seq.sh
+++ seq_to_ncbi_info/bash_info_seq.sh
@@ -149,7 +149,8 @@ do
     NAME_protein=$(cat $VAR | grep 'seq_' | awk -F \t '{print $2}' | tr -d '\\')
     NAME_PROTIEN_INFO=$(cat $VAR | grep seq_ | sed 's/XP/\xx XP/g' | sed 's/\\n/ xx /g'| awk  -F xx '{print $5}')
     NAME_GENE=$( cat $VAR | grep '\<Gene-track_geneid\>' |tr '<' '  ' | tr '>' ' ' | awk '{print $2}')
-    echo -e  $seq \t $NAME_protein \t $NAME_PROTIEN_INFO \t "LOC"$NAME_GENE >> info_seq.txt
+    MRNA_NAME=$(cat $VAR| egrep 'GBSeq_locus>XM_' | tr '>' ' ' | tr '<' ' ' | awk '{print $2}')
+    echo -e  $seq $NAME_protein $NAME_PROTIEN_INFO $NAME_GENE $MRNA_NAME>> info_seq.txt
     wget 'https://www.ncbi.nlm.nih.gov/gene/?term=LOC'$NAME_GENE'&report=gene_table&format=text' -O intro.txt
     cat intro.txt  >> gene_introextro.txt
     cat intro.txt >> $VAR
